2ba13354e71f331507fddef0577be00c8c777a42
Stanislas Daniel Claude Dolcini <stanislas.dolcini@gmail.com>
2024-08-01 15:32:43 +0200

    Try to fix it

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ GitHistoryChannelLogger = conf.registerPlugin('GitHistoryChannelLogger')
 
 # Register dynamic configuration for repositories
 def registerRepo(name):
-    conf.registerGroup(GitHistoryChannelLogger, name)
+    GitHistoryChannelLogger[name] = conf.registerGroup(GitHistoryChannelLogger, name)
     conf.registerGlobalValue(GitHistoryChannelLogger[name], 'url',
         registry.String('', """URL of the repository."""))
     conf.registerGlobalValue(GitHistoryChannelLogger[name], 'branch',
