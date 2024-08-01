5db40d3e6ddcd2571714ad725fc6a4610e6ba38b
Stanislas Daniel Claude Dolcini <stanislas.dolcini@gmail.com>
2024-08-01 16:03:01 +0200

    Try to fix it

--- plugin.py
+++ plugin.py
@@ -47,7 +47,7 @@ class GitHistoryChannelLogger(callbacks.Plugin):
         self.startCommitWatcher()
 
     def loadRepos(self):
-        repos_config = self.registryValue('repos').split()
+        repos_config = self.registryValue('repos')
         for repo in repos_config:
             url = self.registryValue(f'{repo}.url')
             branch = self.registryValue(f'{repo}.branch')
